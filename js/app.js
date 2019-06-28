af707e9ad1e153cfb5728d25219636e90537627d
alexandrurdev <contact@ardev.ro>
2019-06-29 01:46:13 +0300

    added function for auto toggling (depends of hour) || and bug fixing to click event

--- js/app.js
+++ js/app.js
@@ -8,39 +8,41 @@ var moonIcon = document.getElementById('moonIconJS');
 var d = new Date();
 var n = d.getHours();
 
-
-if(n <= 9) {
-	sunIcon.style.display = 'none';
-	moonIcon.style.display = 'block';
-	body.classList.add("darkMode");
-	toggleJS.classList.remove('checked');
-} else {
-	sunIcon.style.display = 'block';
-	moonIcon.style.display = 'none';
-	body.classList.remove("darkMode");
-	toggleJS.classList.add('checked');
-}
-
+autoToggler();
 
 toggler.addEventListener('click', () => {
+	autoToggler = undefined;
 	toggler.classList.toggle('checked');
 	if(toggler.classList.contains('checked')) {
-		console.log('checked');
 		day();
 	} else {
-		console.log('unchecked');
 		night();
 	}
 });
 
 function day() {
-	moonIcon.style.display = 'none';
-	sunIcon.style.display = 'block';
+	sunIcon.style.display = 'none';
+	moonIcon.style.display = 'block';
 	body.classList.add("darkMode");
 }
 
 function night() {
-	sunIcon.style.display = 'none';
-	moonIcon.style.display = 'block';
+	moonIcon.style.display = 'none';
+	sunIcon.style.display = 'block';
 	body.classList.remove("darkMode");
+}
+
+function autoToggler() {
+	if(n <= 9) {
+		sunIcon.style.display = 'none';
+		moonIcon.style.display = 'block';
+		body.classList.add("darkMode");
+		toggleJS.classList.add('checked');
+		toggler.click();
+	} else {
+		sunIcon.style.display = 'block';
+		moonIcon.style.display = 'none';
+		body.classList.remove("darkMode");
+		toggleJS.classList.remove('checked');
+	}
 }
